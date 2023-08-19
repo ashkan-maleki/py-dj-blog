b986512466807481d98834518fb862c79f2b6f6c
Your Name <you@example.com>
2023-08-19 08:13:14 +0000

    add updated time to post model

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
