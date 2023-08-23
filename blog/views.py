8b736256060a6504f574dcf58181e1640002d50c
Your Name <you@example.com>
2023-08-23 08:53:52 +0000

    add loggin to the project

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    logger.debug(type({'posts': posts}))
+    return render(request, template_name='blog/post/list.html', context={'posts': posts})
 
 
 def post_detail(request, id):
@@ -13,7 +18,7 @@ def post_detail(request, id):
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     return Http404("No post found")
-
     post = get_object_or_404(Post,id=id, status=Post.Status.PUBLISHED)
-
-    return render(request, 'blog/post/detail.html', {"post", post})
+    # return render(request, 'blog/post_list.html', {'posts': posts})
+    logger.debug(type({'post': post}))
+    return render(request, template_name='blog/post/detail.html', context={'post': post})
