a40b44d831b527ba86762151a7f7e3c31b7ead5b
Your Name <you@example.com>
2023-08-24 11:19:12 +0000

    add canonical url

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -42,3 +43,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
