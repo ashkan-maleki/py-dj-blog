cfba84f925eaae375e373345a2ff582380442941
Your Name <you@example.com>
2023-08-24 11:27:17 +0000

    change post detail url from id to slug

--- blog/views.py
+++ blog/views.py
@@ -13,12 +13,17 @@ def post_list(request):
     return render(request, template_name='blog/post/list.html', context={'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request, id, year, month, day, post):
     # try:
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     return Http404("No post found")
-    post = get_object_or_404(Post,id=id, status=Post.Status.PUBLISHED)
+    # post = get_object_or_404(Post,id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
     # return render(request, 'blog/post_list.html', {'posts': posts})
     logger.debug(type({'post': post}))
     return render(request, template_name='blog/post/detail.html', context={'post': post})
