8d49670d9cd8e0a4202737126e472223068cd283
Your Name <you@example.com>
2023-08-26 09:45:37 +0000

    fix git problem

---
+++ Makefile
@@ -0,0 +1,86 @@
+all: run
+
+#===============================
+# Environment setup
+#===============================
+
+env-setup:
+	sudo apt install python3.10-venv
+	sudo apt install python3-pip
+
+#===============================
+# Git commands
+#===============================
+
+COMMIT_MESSAGE ?= $(shell bash -c 'read -p "Commit Message: " COMMIT_MESSAGE; echo $$COMMIT_MESSAGE')
+
+#@echo Commit Message › $(COMMIT_MESSAGE)
+commit:
+	git add .
+	git commit -m "$(COMMIT_MESSAGE)"
+
+push:
+	git push
+
+pull:
+	git fetch
+	git pull
+
+
+#USERNAME ?= $(shell bash -c 'read -p "Username: " username; echo $$username')
+#PASSWORD ?= $(shell bash -c 'read -s -p "Password: " pwd; echo $$pwd')
+#
+#
+#
+#talk:
+#	@clear
+#	@echo Username › $(USERNAME)
+#	@echo Password › $(PASSWORD)
+
+#===============================
+# Project setup
+#===============================
+
+pyenv-create:
+	python -m venv venv
+
+freeze:
+	pipreqs . --force
+
+install-py-packages:
+	pip install pipreqs
+	pip install -r requirements.txt
+
+setup: pyenv-create pyenv-activate
+
+
+# don't run this command, because source cannot executed in makefile
+# I keep this command here for convenience
+pyenv-activate:
+	source venv/bin/activate
+
+
+
+pyenv-deactivate:
+	deactivate
+
+#===============================
+# Project run
+#===============================
+
+
+run:
+	python manage.py runserver 127.0.0.1:8000 --settings=weblog.settings
+
+#===============================
+# Project django
+#===============================
+DJANGO_APP ?= $(shell bash -c 'read -p "Django App: " DJANGO_APP; echo $$DJANGO_APP')
+migrate:
+	python manage.py migrate
+
+mk-migration:
+	python manage.py makemigrations $(DJANGO_APP)
+
+shell:
+	python manage.py shell
---
+++ blog/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from django.core.mail import send_mail
+from django.http import Http404
+
+from .forms import EmailPostForm
+from .models import Post
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+
+def post_list(request):
+    post_all = Post.published.all()
+    paginator = Paginator(post_all, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    logger.debug(type({'posts': posts}))
+    return render(request, template_name='blog/post/list.html', context={'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     return Http404("No post found")
+    # post = get_object_or_404(Post,id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED, slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+    # return render(request, 'blog/post_list.html', {'posts': posts})
+    logger.debug(type({'post': post}))
+    return render(request, template_name='blog/post/detail.html', context={'post': post})
+
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com',
+                      [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
