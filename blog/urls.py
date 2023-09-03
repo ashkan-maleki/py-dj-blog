c366fc1eb02feca4fc29ba2b00d0dfc3c86d6af3
ma.maleki <ma.maleki@PC-1293>
2023-09-03 10:20:50 +0330

    change post detail url from slug to str

--- blog/urls.py
+++ blog/urls.py
@@ -15,7 +15,7 @@ class FarsiSlug:
         return value
 
 # register_converter(converters.FarsiSlug, "slugf")
-register_converter(FarsiSlug, "slugf")
+# register_converter(FarsiSlug, "slugf")
 
 urlpatterns = [
     path('', views.post_list, name="post_list"),
@@ -23,7 +23,7 @@ urlpatterns = [
     path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
     path('<int:id>/', views.post_detail, name="post_detail"),
     # path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name="post_detail"),
-    path('<int:year>/<int:month>/<int:day>/<slugf:post>/', views.post_detail, name="post_detail"),
+    path('<int:year>/<int:month>/<int:day>/<str:post>/', views.post_detail, name="post_detail"),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
