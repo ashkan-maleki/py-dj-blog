438033023c42aa35e64b0013d0f612c9ed627a08
ma.maleki <ma.maleki@PC-1293>
2023-09-03 10:26:45 +0330

    make tag url suppurt unicode

--- blog/urls.py
+++ blog/urls.py
@@ -20,7 +20,8 @@ class FarsiSlug:
 urlpatterns = [
     path('', views.post_list, name="post_list"),
     # path('', views.PostListView.as_view(), name="post_list"),
-    path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
+    # path('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
+    path('tag/<str:tag_slug>/',views.post_list, name='post_list_by_tag'),
     path('<int:id>/', views.post_detail, name="post_detail"),
     # path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name="post_detail"),
     path('<int:year>/<int:month>/<int:day>/<str:post>/', views.post_detail, name="post_detail"),
